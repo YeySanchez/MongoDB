a98a84263d14a0b18aaf3cef2555eabd3754a88b
YeySanchez <64381176+YeySanchez@users.noreply.github.com>
2020-10-29 16:57:13 -0500

    v64 Middleware rol admin

--- routes/animal.js
+++ routes/animal.js
@@ -9,22 +9,23 @@ var md_auth = require('../middlewares/authenticaded');
 var multipart = require('connect-multiparty');
 //v63 cargar imagenb  imagen animal 
 var md_upload = multipart({uploadDir:'./uploads/animal'});
-
+//64 middleware animal
+var md_admin = require('../middlewares/is_admin');
 
 api.get('/pruebas-animales',md_auth.ensureAuth, AnimalController.pruebas);
 //metodo guardar animal 
-api.post('/animal',md_auth.ensureAuth, AnimalController.SaveAnimal);
+api.post('/animal',[md_auth.ensureAuth,md_admin.isAdmin], AnimalController.SaveAnimal);
 //metodo listar animal
 api.get('/animals',AnimalController.getAnimals);
 //metodo listar animal unico 
 api.get('/animal/:id',AnimalController.getAnimal);
 //metodo actualizar animal 
-api.put('/animal/:id',md_auth.ensureAuth,AnimalController.updateAnimal);
+api.put('/animal/:id',[md_auth.ensureAuth,md_admin.isAdmin],AnimalController.updateAnimal);
 //v63 ruta devolver imagen animal 
-api.post('/upload-image-animal/:id',[md_auth.ensureAuth,md_upload],AnimalController.uploadImage);
+api.post('/upload-image-animal/:id',[md_auth.ensureAuth,md_admin.isAdmin,md_upload],AnimalController.uploadImage);
 //v63 cargar imagenb  imagen animal 
 api.get('/get-image-animal/:imageFile',AnimalController.getImageFile);
 //v64 eliminar animal
-api.delete('/animal/:id',md_auth.ensureAuth, AnimalController.deleteAnimal);
+api.delete('/animal/:id',[md_auth.ensureAuth,md_admin.isAdmin], AnimalController.deleteAnimal);
 
 module.exports = api;
